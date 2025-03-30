f916ed682ab26af2c02f89cba2c6688010aa4de6
Deepak yadav <deepak19939426@gmail.com>
2025-03-30 23:11:38 +0530

    add info3

--- myfolio/src/Components/Layout/Footer.js
+++ myfolio/src/Components/Layout/Footer.js
@@ -8,10 +8,10 @@ const Footer = () => {
             <div className='footer-container'>
                 <h3>All rights reserved © Deepak Yadav</h3>
                 <div className='social-icons'>
-                    <a href="#" className="icon"><FaFacebookF /></a>
-                    <a href="#" className="icon"><FaTwitter /></a>
-                    <a href="#" className="icon"><FaInstagram /></a>
-                    <a href="#" className="icon"><FaLinkedinIn /></a>
+                    <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="icon"><FaFacebookF /></a>
+                    <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer" className="icon"><FaTwitter /></a>
+                    <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="icon"><FaInstagram /></a>
+                    <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer" className="icon"><FaLinkedinIn /></a>
 
                 </div>
             </div>
